be0eb70e21b83f8fab4edc239f3e3396121db99b
qusirn <18756588842@qq.com>
2018-05-27 09:07:48 +0000

    Add user following

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -10,9 +10,13 @@
             <h3>微博列表</h3>
             @include('shared._feed')
         </div>
+
         <aside class="col-md-4">
             <section class="user_info">
                 @include('shared._user_info', ['user' => Auth::user()])
+            </section>
+            <section class="stats">
+                @include('shared._stats', ['user' => Auth::user()])
             </section>
         </aside>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,9 @@ Route::delete('logout', 'SessionsController@destroy')->name('logout');
 
 // 微博
 Route::resource('statuses', 'StatusesController', ['only' => ['store', 'destroy']]);
+
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
+
+Route::post('/users/followers/{user}', 'FollowersController@store')->name('followers.store');
+Route::delete('/users/followers/{user}', 'FollowersController@destroy')->name('followers.destroy');
